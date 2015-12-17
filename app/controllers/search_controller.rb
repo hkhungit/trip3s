6205e1bc57ab624f95e526924ed32831023d5e22
quocdat <dangquocdat.ht@gmail.com>
2015-12-18 01:51:00 +0700

    chi tiet dia diem

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,6 +5,6 @@ class SearchController < ApplicationController
     @posts  = Post.select("*").joins(:place).order('post_view desc').limit(4)
     @places_category = Category.select("*").joins(:type).where(:types => {:type_name => 'type_category_place'}).limit(10)
     @places_cate_all=Category.select("*").joins(:type).where(:types => {:type_name => 'type_category_place',:type_name => 'type_city',:type_name => 'type_district',:type_name => 'type_area',:type_name => 'type_diding_place',:type_name => 'type_property_place',:type_name => 'type_cuisine_place',:type_name => 'type_purpose_place'}).limit(10)
-    @places_two_limit_one=Post.find_by_sql ["select A.post_title, A.post_review,A.post_view,B.place_choice,B.place_address,A.post_url,A.post_thumbnail from posts A,places B WHERE A.post_title like ? and A.id=B.post_id and A.id in (select id FROM posts ORDER by post_review DESC) ORDER by B.place_choice DESC limit 10", "%#{params[:search]}%"]
+    @places_two_limit_one=Post.find_by_sql ["select A.id,A.post_title, A.post_review,A.post_view,B.place_choice,B.place_address,A.post_url,A.post_thumbnail from posts A,places B WHERE A.post_title like ? and A.id=B.post_id and A.id in (select id FROM posts ORDER by post_review DESC) ORDER by B.place_choice DESC limit 10", "%#{params[:search]}%"]
   end
 end
